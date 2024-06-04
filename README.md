54ee9017a6fe912ae7680e37b671e971afb6af87
Colin MacRae <76566137+comacrae@users.noreply.github.com>
2024-06-04 11:08:29 -0500

    added sqlalchemy

---
+++ README.md
@@ -0,0 +1,5 @@
+Frontend : React
+Backend: FastAPI
+Authorization Schema: OAuth-2 Password config
+ORM: SQLAlchemy
+DB: SQLite
---
+++ sql_app/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, UniqueConstraint
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import PrimaryKeyConstraint
+
+from .database import Base
+
+class User(Base):
+  __tablename__ = "users"
+  id = Column(Integer, primary_key=True)
+  username = Column(String, unique=True)
+  email = Column(String,unique=True,index=True)
+  hashed_password= Column(String)
+  is_active= Column(Boolean, default=True)
+
+  posts = relationship("Post", back_populates="owner")
+  subscriptions = relationship("User", back_populates="subscribers")
+
+class Post(Base):
+  __tablename__ = "posts"
+  id = Column(Integer, primary_key = True)
+  owner = relationship("User", back_populates="posts")
+  items  = relationship("PostItem", back_populates="containing_post")
+
+class PostItem(Base):
+  __tablename__ = "post_items"
+  id= Column(Integer, primary_key=True)
+  item = Column(String, max_length=255)
+  containing_post = relationship(Post, back_populates="items")
+
+class Subscription(Base):
+  subscription_id = Column(Integer, primary_key=True) # user who is being followed
+  subscriber_id = Column(Integer, primary_key=True) # the following user
+  subcribers = relationship("User", back_populates="subscriptions")
+  __table_args__ = (
+    PrimaryKeyConstraint('subscription_id','subscriber_id')
+  )
+  
+
