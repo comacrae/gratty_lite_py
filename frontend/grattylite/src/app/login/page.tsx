6caddfdbd7279da2a838ec2b4f5d5c7dd4936a76
Colin MacRae <76566137+comacrae@users.noreply.github.com>
2024-06-10 20:58:38 -0500

    starting to work on front end login page

--- frontend/grattylite/src/app/login/page.tsx
+++ frontend/grattylite/src/app/login/page.tsx
@@ -1,15 +1,22 @@
+interface LoginInfo {
+  username: string;
+  password: string;
+}
+
 export default function LoginPage() {
   return (
-    <main>
-      <form>
-        <input type="text" id="username">
-          Username
-        </input>
-        <input type="password" id="password">
-          Password
-        </input>
-        <button type="submit">Login</button>
+    <main className="bg-100">
+      <form action="http://localhost:8000/auth/login" method="POST">
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+        <button className="btn" type="submit">
+          Login
+        </button>
       </form>
     </main>
   );
 }
+
+async function attemptLogin(credentials: LoginInfo) {}
